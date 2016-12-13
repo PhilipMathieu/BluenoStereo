106c22ba42bc92d6f1c37c6653fde7a7a4fcb134
PhilipMathieu <philip.eng.mathieu@gmail.com>
2016-12-13 13:27:36 -0500

    Added arduino code to repository

--- shutdownmanager.ino
+++ shutdownmanager.ino
@@ -1,23 +1,53 @@
 #include "SleepyPi2.h"
 #include <LowPower.h>
- 
+
+HANDSHAKE_IN = 7;
+HANDSHAKE_OUT = 17;
+RELAY = 16;
+ACCPWR = 5;
+
+void wake_arduino(){
+  // just a placeholder - code then resumes following interrupt
+}
+
 void setup() {
  
-   // Configure "Standard" LED pin
-   pinMode(17, OUTPUT); 
-   digitalWrite(LED_PIN,LOW); // Switch off LED
+  // Configure pins
+  pinMode(HANDSHAKE_OUT, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(HANDSHAKE_IN, INPUT);
+  pinMode(ACCPWR, INPUT);
+
+  // Add interrupt to detect accessory power
+  attachInterrupt(ACCPWR, wake_arduino, RISING);
 }
- 
-void loop() {
- 
-    // Enter idle state for 8 s with the rest of peripherals turned off
-    SleepyPi.idle(SLEEP_8S, ADC_OFF)
- 
-   // Do something here
-   // Example: Read sensor, data logging, data transmission.
-   Serial.println("I've Just woken up");
-   digitalWrite(17,HIGH); // Switch on LED
-   delay(350); 
-   digitalWrite(17,LOW); // Switch off LED
- 
+
+void sleepnow(){
+  digitalWrite(RELAY, LOW);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  attachInterrupt(ACCPWR,wake_arduino, RISING);
+  sleep_mode();
+  sleep_disable();
+  detach_interrupt(ACCPWR);
+  digitalWrite(RELAY, HIGH);
 }
+
+void loop(){
+  // A start of loop we must be powered up
+  if (digitalRead(ACCPWR) == LOW){
+    // tell Pi to power off
+    digitalWrite(HANDSHAKE_OUT, LOW);
+    // give it up to 30 secs
+    int count = 0;
+    while (count < 30000){
+      // if Pi shutdown
+      if (digitalRead(HANDSHAKE_IN) == LOW){
+        break;
+      }
+      count++;
+    }
+    // Go into low power mode
+    sleepnow();
+  }
+}  
